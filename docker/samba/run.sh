f19176ab1b7ddc0a40393a8cbbb8d1c17b27c3ce
Shinsuke Sugaya <shinsuke@apache.org>
2025-04-13 10:22:14 +0900

    update settings

--- docker/samba/run.sh
+++ docker/samba/run.sh
@@ -6,8 +6,10 @@ cat <<EOS
 Name: Test
 
 Path:
-smb://localhost/public/
-smb1://localhost/public/
+smb://localhost:1139/public/
+smb1://localhost:1139/public/
+smb://localhost:1139/users/
+smb1://localhost:1139/users/
 
 Authentication:
 Username: testuser1 or testuser2
@@ -21,13 +23,17 @@ if [ x"$USE_SMB1" != "xfalse" ] ; then
 fi
 
 rm -rf data
-mkdir data
-cp -r ../docuworks ../ichitaro ../msoffice ../other ../source_code ../xml ../autocad ../pdf ../text ../archive ../html data
+mkdir -p data/share data/users
+for f in $(find ../../docuworks ../../ichitaro ../../msoffice ../../other ../../source_code ../../xml ../../autocad ../../pdf ../../text ../../archive ../../html -type f) ; do
+  cp $f data/share/share_$(basename $f)
+  cp $f data/users/users_$(basename $f)
+done
+exit
 
-docker run -it -p 139:139 -p 445:445 -v `pwd`/data:/share dperson/samba $SMB_OPTIONS \
+docker run -it -p 1139:139 -p 1445:445 -v `pwd`/data:/share dperson/samba $SMB_OPTIONS \
             -u "testuser1;test123" \
             -u "testuser2;test123" \
             -s "public;/share" \
-            -s "users;/srv;no;no;no;testuser1,testuser2" \
+            -s "users;/users;no;no;no;testuser1,testuser2" \
             -s "testuser1 private share;/testuser1;no;no;no;testuser1" \
             -s "testuser2 private share;/testuser2;no;no;no;testuser2"
